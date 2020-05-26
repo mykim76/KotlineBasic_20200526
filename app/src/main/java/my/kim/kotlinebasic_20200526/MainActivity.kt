aecf07e75750b4c44562fc0b5becb278da0ecc6d
mykim76 <iyoungfm@gmail.com>
2020-05-26 21:59:52 +0900

    버튼이 눌리면 editTest 내용을 추출하여 textView에 출력

--- app/src/main/java/my/kim/kotlinebasic_20200526/MainActivity.kt
+++ app/src/main/java/my/kim/kotlinebasic_20200526/MainActivity.kt
@@ -2,11 +2,19 @@ package my.kim.kotlinebasic_20200526
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        okBtn.setOnClickListener {
+
+            val inputContent= contentEdt.text.toString()
+            resultTxt.text = inputContent
+
+        }
     }
 }
